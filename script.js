f296f196c1cea4f2a2c48c965dfde594136d0a82
Greg-makes-stuff <gregsgit@proton.me>
2025-04-10 19:49:57 -0300

    add layout

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-document.getElementById("clickMeBtn").addEventListener("click", function () {
-  const audio = new Audio("sounds/chica.mp3");
-  audio.play();
+const SOUNDS_DIR = "sounds";
+const sounds = [
+  { name: "Chika Chikaaa", file: "chica.mp3" },
+  { name: "BOW BOW", file: "bowbow.mp3" },
+  {
+    name: "SCREAMING",
+    file: "screaming.wav",
+  },
+];
+
+const container = document.getElementById("buttonContainer");
+
+sounds.forEach((sound, index) => {
+  const button = document.createElement("button");
+  button.textContent = sound.name;
+  button.addEventListener("click", () => {
+    const audio = new Audio(`${SOUNDS_DIR}/${sound.file}`);
+    audio.play();
+  });
+  container.appendChild(button);
 });
